9a756c4ffb330cdfe9f4280df6837c9edd667ac2
Bruno Bonamin <bruno@bonamin.org>
2022-10-08 23:28:55 -0300

    Set up tailwind and landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,30 @@
+import bgImage from "../images/bg.jpg";
+import logoImage from "../images/logo.png";
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="grid grid-rows-3 h-screen w-screen">
+      <div className="flex items-center justify-center">
+        <img
+          className="object-scale-down h-16 w-16 md:h-36 md:w-36"
+          src={logoImage}
+          alt="Baby Okapi Logo"
+        />
+      </div>
+      <div className="flex items-center justify-center">
+        <h1 className="font-semibold text-3xl md:text-6xl text-gray-600">
+          COMING SOON
+        </h1>
+      </div>
+      <div className="row-start-3 flex items-center justify-center">
+        <a
+          href="https://www.instagram.com/babyokapi.eu/"
+          className="bg-rose-400 hover:bg-rose-500 text-white font-bold py-2 px-4 border-b-4 border-rose-700 hover:border-rose-500 rounded"
+        >
+          Instagram
+        </a>
+      </div>
+      <img className="top-0 absolute -z-10 h-screen w-screen" src={bgImage} />
     </div>
   );
 }
