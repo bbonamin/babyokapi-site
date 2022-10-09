4e295899096b5fe27698e317e565b246372aaa18
Bruno Bonamin <bruno@bonamin.org>
2022-10-08 23:47:45 -0300

    Fix favicon, title, readme

--- app/root.tsx
+++ app/root.tsx
@@ -8,16 +8,21 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import logoImage from "~/images/logo.png";
 import styles from "./styles/app.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "BabyOkapi.eu",
   viewport: "width=device-width,initial-scale=1",
 });
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [
+    { rel: "shortcut icon", href: logoImage, type: "image/x-icon" },
+    { rel: "icon", href: logoImage, type: "image/x-icon" },
+    { rel: "stylesheet", href: styles },
+  ];
 }
 
 export default function App() {
